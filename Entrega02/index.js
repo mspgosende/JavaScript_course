1eeccba9843d9e6c8092bc522cd7cc979eafeeed
Maria Sol Gosende <msp.01209@gmail.com>
2022-09-14 00:41:41 -0300

    Entrega 02

---
+++ Entrega02/index.js
@@ -0,0 +1,5 @@
+let precioDePrenda = parseInt(prompt('Ingrese cantidad de prendas')) 
+let cantDeCuotas = parseInt(prompt('Ingrese cantidad de cuotas'))  
+
+
+alert ('Total: '+ (precioDePrenda / cantDeCuotas))
